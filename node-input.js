b3f07539d4cdf98f00efc26a832160e391b3ee3e
David Gent <gent@larkstoungue-ub.(none)>
2012-07-06 17:29:25 -0700

    wesocket module added

--- node-input.js
+++ node-input.js
@@ -4,6 +4,7 @@ var http = require('http');
 var fs = require('fs');
 var util = require('util');
 var optimist = require('optimist');  // npm installed module for option parsing
+var ws = require("websocket");       // npm installed module for WebSockets
 
 // Defaults
 serverPortNum = 8081;
